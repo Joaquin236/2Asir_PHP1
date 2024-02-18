55089423e05e58d5c8dc40504f7a468f042d245a
Joaquin236 <joaquin_pinguinal@hotmail.com>
2024-02-18 19:15:50 +0100

    Update prog3.php

--- Hojas_PHP/Hoja1_php/prog3.php
+++ Hojas_PHP/Hoja1_php/prog3.php
@@ -10,10 +10,10 @@ $saldo=12312;
 //---------------------
 
 // kernel
-function Evaluator($europeo, $casado, $mujer, $edad, $saldo) {
-    if ($europeo==1 and $casado==0) {
+function Evaluator($europeo, $casado, $mujer, $edad, $saldo) { // declare (args)
+    if ($europeo==1 and $casado==0) { // case1 --> $europeo ==1 && $casado==0
         echo "// DEBUG: ERES EUROPEO Y NO CASADO";
-    }elseif ($saldo>10000 or $europeo==0 and $edad%4==0) {
+    }elseif ($saldo>10000 or $europeo==0 and $edad%4==0) { // case2 --> saldo>10000 or $europeo==0 && edad % 4 == 0
         echo "// DEBUG: NO ERES EUROPEO, SU SALDO SUPERA LOS 10000 Y SU EDAD ES DIVISIBLE ENTRE 4";
     }
     
