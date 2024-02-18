2da87d27004543e878448a617d3885f1c50cc1cf
Joaquin236 <joaquin_pinguinal@hotmail.com>
2024-02-18 17:40:59 +0100

    Update prog2.php

--- Hojas_PHP/Hoja1_php/prog2.php
+++ Hojas_PHP/Hoja1_php/prog2.php
@@ -10,11 +10,15 @@ $caracter="V";
 //----------------------------------
 
 # kernel
-function Evaluator($nota, $age, $caracter) {
-    if ($nota=>5 and $age=>18 and $caracter=="M" ) {
+function Evaluator($nota, $age, $caracter) { // declare (args)
+    if ($nota>=5 and $age>=18 and $caracter=="M" or $caracter=="m") { //case1--> nota(+5) edad(+18) letra(M o m)
         echo "// DEBUG: ACEPTADA";
-    }elseif ($nota=>5 and $age>=18 and $caracter=="V") {
+    }elseif ($nota>=5 and $age>=18 and $caracter=="V" or $caracter=="v") { //case2--> nota(+5) edad(+18) letra(V o v)
         echo "// DEBUG: APEPTADO";
+    }elseif ($nota>=5 and $age<=18) { // case3--> nota(+5) edad(-18)
+        echo "// CAUTION: NO ES POSIBLE. ES MENOR DE EDAD";
+    }elseif (nota<=5) { //case4--> nota(-5)
+        echo "// WARNING: NO ES POSIBLE. NOTA SUSPENSA";
     }
     
 }
@@ -23,7 +27,4 @@ function Evaluator($nota, $age, $caracter) {
 
 # interface
 Evaluator($nota, $age, $caracter);
-
-
-
 ?>
