ddc3ab89469dda1aca2793834239f88af5b64cf7
Joaquin236 <joaquin_pinguinal@hotmail.com>
2024-03-24 16:49:20 +0100

    24-03

--- Hojas_PHP/Hoja2_php/prog6.php
+++ Hojas_PHP/Hoja2_php/prog6.php
@@ -16,25 +16,26 @@ $sueldo8=2005;
 
 //kernel
 function Proceso($sueldo1,$sueldo2,$sueldo3,$sueldo4,$sueldo5,$sueldo6,$sueldo7,$sueldo8) {
+    $a=1;
+    while ($a <= 8) {
+        if ($sueldo1 >= 600 || $sueldo2 >= 600 || $sueldo3 >= 600 || $sueldo4 >= 600 || $sueldo5 >= 600 || $sueldo6 >= 600 ||  $sueldo7 >= 600 || $sueldo8 >= 600) {
+            $Sueldo_bajo = "Los sueldos bajos son --> " . $a;
+        }
+    $a++;
+    }
+    return $a;
+}
 
+//interface
+function Visual($data) {
+    echo $data;
     
 }
 
+//call kernel
+$result=Proceso($sueldo1,$sueldo2,$sueldo3,$sueldo4,$sueldo5,$sueldo6,$sueldo7,$sueldo8);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//call interface
+Visual($result);
 
 ?>
